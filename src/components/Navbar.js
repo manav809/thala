565572530ba954a82b5d7ac02de672314859f82f
manav809 <manavpatel6978@gmail.com>
2023-12-24 11:19:33 -0500

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ShareIcon from '@mui/icons-material/Share';
+
+export default function MenuAppBar() {
+  return (
+    <Box>
+      <AppBar position="static" color="inherit" enableColorOnDark>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            #Thala          
+          </Typography>
+          <div>
+            <IconButton size="small" color="inherit">
+              <DarkModeIcon />
+            </IconButton>
+            <IconButton size="small" color="inherit">
+              <GitHubIcon />
+            </IconButton>
+            <IconButton size="small" color="inherit">
+              <ShareIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
