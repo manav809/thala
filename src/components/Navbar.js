9dc12e21d9c96ead8d344399d3a323d801ce721d
manav809 <manavpatel6978@gmail.com>
2023-12-26 23:11:53 -0500

    Add Tooltip and Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,43 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import ShareIcon from '@mui/icons-material/Share';
-import LightModeIcon from '@mui/icons-material/LightMode';
-export default function MenuAppBar({change, darkMode}) {
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import ShareIcon from "@mui/icons-material/Share";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { Tooltip } from "@mui/material";
+export default function MenuAppBar({ change, darkMode }) {
   return (
     <Box>
       <AppBar position="static" color="inherit" enableColorOnDark>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            #Thala          
+            #Thala
           </Typography>
           <div>
-            <IconButton size="medium" color="inherit" onClick={change} darkMode={darkMode}>
-              {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
-            </IconButton>
-            <IconButton size="medium" color="inherit">
-              <GitHubIcon />
-            </IconButton>
-            <IconButton size="medium" color="inherit">
-              <ShareIcon />
-            </IconButton>
+            <Tooltip title="color">
+              <IconButton
+                size="medium"
+                color="inherit"
+                onClick={change}
+                darkMode={darkMode}
+              >
+                {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="github">
+              <IconButton size="medium" color="inherit" href="http://www.github.com/manav809/thala" >
+                <GitHubIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="share">
+              <IconButton size="medium" color="inherit">
+                <ShareIcon />
+              </IconButton>
+            </Tooltip>
           </div>
         </Toolbar>
       </AppBar>
